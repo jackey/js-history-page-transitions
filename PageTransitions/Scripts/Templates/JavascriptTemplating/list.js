9f27bfea426c08d977d56cda41b359b600797be7
Eric Sowell <eric.sowell@gmail.com>
2011-10-10 17:21:31 -0500

    cheated and made the subviews on the list page easy

--- PageTransitions/Scripts/Templates/JavascriptTemplating/list.js
+++ PageTransitions/Scripts/Templates/JavascriptTemplating/list.js
@@ -1,15 +1,24 @@
-﻿define(['/scripts/text.js!/scripts/Templates/JavascriptTemplating/list.htm'], function (template) {
-    return {
+﻿define(['/scripts/text.js!/scripts/Templates/JavascriptTemplating/list.htm',
+        '/scripts/text.js!/scripts/Templates/JavascriptTemplating/list_sub.htm'],
+        function (template, subtemplate) {
+            return {
 
-        template: function (targetSelector, model) {
-            console.log('targetSelector', targetSelector);
-            console.log('element', $(targetSelector));
+                template: function (targetSelector, model) {
+                    console.log('targetSelector', targetSelector);
+                    console.log('element', $(targetSelector));
 
-            var compiled = _.template(template);
-            var text = compiled(model);
-            console.log('text of list', text);
+                    var outerTemplate = _.template(template);
+                    var itemTemplate = _.template(subtemplate);
 
-            $(targetSelector).html(text);
-        }
-    }
-});
+                    var items = '';
+
+                    _.each(model, function (item) {
+                        items += itemTemplate(item);
+                    });
+
+                    var text = outerTemplate({ model: model, list: items });
+
+                    $(targetSelector).html(text);
+                }
+            }
+        });
