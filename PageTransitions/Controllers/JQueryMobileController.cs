387eef434b5308f81898053cab5afe7e3bc6ceab
Eric Sowell <eric.sowell@gmail.com>
2011-10-13 15:41:10 -0500

    added jquery mobile

---
+++ PageTransitions/Controllers/JQueryMobileController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PageTransitions.Controllers
+{
+    public class JQueryMobileController : SharedController
+    {
+        private PersonInfoRepository _repo = new PersonInfoRepository();
+
+        public ActionResult Index()
+        {
+            var peeps = _repo.GetPersonInfoList();
+
+            return HistoryView("Index", peeps);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var peep = _repo.GetPerson(id.Value);
+
+            return HistoryView("Edit", peep);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id, string name, int? age)
+        {
+            var peep = _repo.GetPerson(id.Value);
+            peep.Name = name;
+            if (age.HasValue)
+                peep.Age = age.Value;
+
+            _repo.SavePerson(peep);
+
+            return RedirectToAction("View", new { id = peep.Id });
+        }
+
+        public ActionResult View(int? id)
+        {
+            var peep = _repo.GetPerson(id.Value);
+
+            return HistoryView("View", peep);
+        }
+
+    }
+}
